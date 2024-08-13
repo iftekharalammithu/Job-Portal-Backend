0973c1dd5686f9e8f620db232bf3b1b493952f3c
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-08-13 19:10:07 +0600

    "Added initial Express.js server setup with dependencies and configured CORS"

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://your-frontend-domain.com", // Replace with your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credential: true,
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
